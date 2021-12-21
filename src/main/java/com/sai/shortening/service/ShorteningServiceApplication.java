3f3fd5ba52f86f1524db27c51fa01e2a7385f2b6
Saikrishna Akkenapelly <saikrishna.akkenapelly@gmail.com>
2021-12-21 17:25:29 +0530

    disabled eureka client to check atleast normal web service is working fine in heorku

--- src/main/java/com/sai/shortening/service/ShorteningServiceApplication.java
+++ src/main/java/com/sai/shortening/service/ShorteningServiceApplication.java
@@ -2,10 +2,8 @@ package com.sai.shortening.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ShorteningServiceApplication
 {
 	public static void main(String[] args)
